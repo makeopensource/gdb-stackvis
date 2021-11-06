8d098e4956b9b28199c91f7adab7797553b5e277
Jake Savitt <64929329+jakesavi@users.noreply.github.com>
2021-11-06 19:00:28 -0400

    MORE ASTHETIC

--- setup.sh
+++ setup.sh
@@ -16,4 +16,4 @@ else
 	touch ~/.gdbinit
 fi
 echo "source gdb-stackvis/stackvis.py" >> ~/.gdbinit # Runs the proper file for execution. If this is inserted into .gdbinit, it will always start.
-echo "DONE DO NOT RUN THIS .sh FILE AGAIN. YOU CAN SAFELY DELETE THIS FILE."
+echo "DONE!!! DO NOT RUN THIS .sh FILE AGAIN. YOU CAN SAFELY DELETE THIS FILE."
