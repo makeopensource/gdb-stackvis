aa8df8a88d056ca52c911dbf18e25ae72076d96c
Jake <jakesavi@buffalo.edu>
2021-11-06 20:20:16 -0400

    fixed the grep syntax.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Building..."
+echo "Building"
 
 if  ! hash gdb ; then
 	echo "Could not find gdb in $PATH"
@@ -15,16 +15,11 @@ then
 else
 	touch ~/.gdbinit
 fi
-<<<<<<< HEAD
 #echo "source gdb-stackvis/stackvis.py" >> ~/.gdbinit # Runs the proper file for execution. If this is inserted into .gdbinit, it will always start.
-if [ ! grep -q source gdb-stackvis/stackvis.py "~/.gdbinit" ];then #checks to see if the string already exists in .gdbinit. If it does DON'T DO IT AGAIN!!
+if  ! grep -q "source gdb-stackvis/stackvis.py" ~/.gdbinit;then #checks to see if the string already exists in .gdbinit. If it does DON'T DO IT AGAIN!!
 	echo "source ./gdb-stackvis/stackvis.py" >> ~/.gdbinit 
 else
-	echo "Already configured!."
+	echo "Already configured!"
 fi
 
 echo "DONE!"
-=======
-echo "source gdb-stackvis/stackvis.py" >> ~/.gdbinit # Runs the proper file for execution. If this is inserted into .gdbinit, it will always start.
-echo "DONE!!! DO NOT RUN THIS .sh FILE AGAIN. YOU CAN SAFELY DELETE THIS FILE."
->>>>>>> dce76c8e907a16de9614ae5eef38d93ded8ed2b1
