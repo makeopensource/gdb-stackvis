9d74e75505e0c34f352707336c7e95508f0d2244
Emil Kovacev <emilkovacev@gmail.com>
2021-11-07 10:42:35 -0500

    fixed location of stackviz python file

--- setup.sh
+++ setup.sh
@@ -17,16 +17,11 @@ else
 	touch ~/.gdbinit
 	echo "success | .gdbinit created"
 fi
-#echo "source gdb_stackviz/stackviz.py" >> ~/.gdbinit # Runs the proper file for execution. If this is inserted into .gdbinit, it will always start.
 
-# UNAME=$(uname -m)
-# if ![[ UNAME = "x86-64" ]] ;then
-#	string manipulate our python file.
-# 	sed -i 's/esp/rsp/g' ./gdb_stackviz/stackviz.py #stack pointer variable for Arm systems go here. If they have an alt system. They can edit the python file themselves
-# 	rm check.txt
-# fi
-if  ! grep -q "source gdb_stackviz/stackviz.py" ~/.gdbinit;then #checks to see if the string already exists in .gdbinit. If it does DON'T DO IT AGAIN!!
-	echo "source ./gdb_stackviz/stackviz.py" >> ~/.gdbinit
+sudo cp -r gdb_stackviz /usr/bin
+
+if  ! grep -q "source /usr/bin/gdb_stackviz/stackviz.py" ~/.gdbinit;then #checks to see if the string already exists in .gdbinit. If it does DON'T DO IT AGAIN!!
+	echo "source /usr/bin/gdb_stackviz/stackviz.py" >> ~/.gdbinit
 	echo "success | configured starting script" 
 else
 	echo "success | already configured"
