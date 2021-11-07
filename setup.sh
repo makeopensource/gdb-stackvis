34eec4d548b6982977493921c0df76affd42bd87
Emil Kovacev <emilkovacev@gmail.com>
2021-11-07 09:48:28 -0500

    modified setup script
    - verified functionality
    - removed unnecessary functionality
    - added print confirmations

--- setup.sh
+++ setup.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-echo "Building"
+echo "building..."
 
 if  ! hash gdb ; then
-	echo "Could not find gdb in $PATH"
+	echo "Error: Could not find gdb in $PATH"
 	exit
 else
 	
-	echo "gdb instillation found!"
+	echo "success | gdb installation found"
 fi
 
 if [ -f "~/.gdbinit" ];
 then
-	echo ".gdbinit file found."
+	echo "success | .gdbinit file found."
 else
 	touch ~/.gdbinit
+	echo "success | .gdbinit created"
 fi
 #echo "source gdb_stackviz/stackviz.py" >> ~/.gdbinit # Runs the proper file for execution. If this is inserted into .gdbinit, it will always start.
 
-uname -m > check.txt
-if ! grep -q "x86-64" check.txt;then
-	#string manipulate our python file.
-	sed -i 's/esp/rsp/g' ./gdb_stackviz/stackviz.py #stack pointer variable for Arm systems go here. If they have an alt system. They can edit the python file themselves
-	rm check.txt
-fi
+# UNAME=$(uname -m)
+# if ![[ UNAME = "x86-64" ]] ;then
+#	string manipulate our python file.
+# 	sed -i 's/esp/rsp/g' ./gdb_stackviz/stackviz.py #stack pointer variable for Arm systems go here. If they have an alt system. They can edit the python file themselves
+# 	rm check.txt
+# fi
 if  ! grep -q "source gdb_stackviz/stackviz.py" ~/.gdbinit;then #checks to see if the string already exists in .gdbinit. If it does DON'T DO IT AGAIN!!
-	echo "source ./gdb_stackviz/stackviz.py" >> ~/.gdbinit 
+	echo "source ./gdb_stackviz/stackviz.py" >> ~/.gdbinit
+	echo "success | configured starting script" 
 else
-	echo "Already configured!"
+	echo "success | already configured"
 fi
+
+echo "config successfully completed"
