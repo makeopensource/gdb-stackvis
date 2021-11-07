235294ece99d8c7ad93fe304b30b377c54db0955
John <jmabramo@buffalo.edu>
2021-11-06 20:09:33 -0400

    move visualize

--- gdb-stackvis/stackvis.py
+++ gdb-stackvis/stackvis.py
@@ -1,6 +1,5 @@
 import gdb
 
-
 class StackVis (gdb.Command):
   """Visualize the stack."""
 
@@ -22,6 +21,33 @@ class StackData:
     def __init__(self, rbp, rsp):
         self.rbp = rbp
         self.rsp = rsp
+
+def visualize(data):
+    height: int = (data.rsp - data.rbp) // 8
+    print(height)
+    if data.rbp == data.rsp:
+        print("No stack data found!")
+        return
+    for i in range(height):
+        if i == 0:
+            print(f"                |---------------------------------------------|\n")
+            print(f"                |                                             |\n")
+            print(f" {hex(data.rbp)} |   0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00   |\n")
+            print(f"                |                                             |\n")
+            print(f"                |---------------------------------------------| <--- rbp\n")
+        #big loopy
+        data.rbp = data.rbp + 0x4
+        print(f"                |                                             |\n")
+        print(f" {hex(data.rbp)} |   0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00   |\n")
+        print(f"                |                                             |\n")
+        print(f"                |---------------------------------------------|\n")
+
+        if i == height - 1:
+            print(f"                |                                             |\n")
+            print(f" {hex(data.rsp)} |   0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00   |\n")
+            print(f"                |                                             |\n")
+            print(f"                |---------------------------------------------| <-- rsp\n")
+
 
 
 StackVis ()
